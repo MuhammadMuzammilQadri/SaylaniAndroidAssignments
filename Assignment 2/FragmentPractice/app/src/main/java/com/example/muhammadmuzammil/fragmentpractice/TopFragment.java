3489b5a2242d1488095ac62477ea1b4972c12308
Muhammad-Muzammil <muhammadmuzammilqadri@live.com>
2015-06-22 01:21:40 +0500

    22nd June Assignment..

---
+++ Assignment 2/FragmentPractice/app/src/main/java/com/example/muhammadmuzammil/fragmentpractice/TopFragment.java
@@ -0,0 +1,43 @@
+package com.example.muhammadmuzammil.fragmentpractice;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class TopFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView= inflater.inflate(R.layout.fragment_top, container, false);
+
+        Button button= (Button) rootView.findViewById(R.id.buttonToReplaceFragment);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.centerFragment, new BottomFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        return rootView;
+    }
+
+
+}
